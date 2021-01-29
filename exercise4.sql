6eef586aaa10ee5f0b3797a9390eae27e82822c5
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-29 12:00:31 +0300

    Add last query on solution

--- exercise4.sql
+++ exercise4.sql
@@ -1,4 +1,4 @@
--- 4 SELECT within SELECT --> In which we form queries using other queries.
+-- Eercise4 SELECT within SELECT --> In which we form queries using other queries.
 
 -- 1. List each country name where the population is larger than that of 'Russia'.
 SELECT name FROM world  WHERE population > (SELECT population FROM world WHERE name='Russia');
