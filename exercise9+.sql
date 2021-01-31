26ff116212de4546dbae7e7e7c711a87605e105d
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-31 09:45:47 +0300

    Modify covid queries

--- exercise9+.sql
+++ exercise9+.sql
@@ -22,4 +22,28 @@ SELECT name, DATE_FORMAT(whn, '%Y-%m-%d'),
   confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER by whn) AS New
   FROM covid WHERE name = 'Italy' AND WEEKDAY(whn) = 0 ORDER BY whn;
 
-  
+-- 5. You can JOIN a table using DATE arithmetic. This will give different results if data is missing.
+-- Show the number of new cases in Italy for each week - show Monday only.
+
+SELECT tw.name, DATE_FORMAT(tw.whn,'%Y-%m-%d'), 
+ tw.confirmed - lw.confirmed
+ FROM covid tw LEFT JOIN covid lw ON 
+  DATE_ADD(lw.whn, INTERVAL 1 WEEK) = tw.whn
+   AND tw.name=lw.name
+WHERE tw.name = 'Italy' AND WEEKDAY(tw.whn) = 0
+ORDER BY tw.whn
+
+-- 6. Notice that while Spain has the second highest confirmed cases, Italy has the second highest number of deaths due to the virus.
+-- Include the ranking for the number of deaths in the table.
+SELECT name, confirmed, RANK() OVER (ORDER BY confirmed DESC) rc, deaths, RANK() OVER (ORDER BY deaths DESC) rc
+  FROM covid WHERE whn = '2020-04-20' ORDER BY confirmed DESC
+
+--   7. Show the infect rate ranking for each country. Only include countries with a population of at least 10 million.
+SELECT world.name,
+   ROUND(100000*confirmed/population,0),
+   RANK() OVER (ORDER BY (confirmed/population) ASC)
+FROM covid JOIN world ON covid.name=world.name
+WHERE whn = '2020-04-20' AND population > 10000000 ORDER BY population DESC;
+
+8. 
+
