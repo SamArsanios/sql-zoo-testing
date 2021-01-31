d005196c40c740d7672c3029d140f1c82cd1b04e
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-31 11:29:17 +0300

    Add queries for nested select quiz

--- quizzes/3_select_quiz.sql
+++ quizzes/3_select_quiz.sql
@@ -1,4 +1,4 @@
---2-- SELECT FROM Quiz -- Nobel Quiz
+--3-- SELECT FROM Quiz -- Nobel Quiz
 
 -- 1. Pick the code which shows the name of winner's names beginning with C and ending in n
 SELECT winner FROM nobel WHERE winner LIKE 'C%' AND winner LIKE '%n'
